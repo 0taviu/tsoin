ba6da76714d7460b6e931d14b442df2130612af5
Otavio <otavioarc@outlook.com>
2022-06-27 23:55:56 -0300

    Adding block class and genesis block

---
+++ src/block.ts
@@ -0,0 +1,31 @@
+import CryptoJS = require('crypto-js');
+
+export class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public timestamp: number;
+  public data: string;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+  }
+
+  calculateHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {Block} from './block';
+
+const genesisBlock: Block = new Block(
+  0,
+  '6d1e1b77089abad23f1290bb0f00b79d988405ec6d3ef17c933c4403bf29b089',
+  '',
+  1656384069,
+  'genesis block'
+);
